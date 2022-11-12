feb8cdeca4a32401567cf02faf23cd76b1b2b158
Linn <115900696+linntwc@users.noreply.github.com>
2022-11-12 02:26:19 -0300

    probleminha resolvido

--- src/botEvents/messageCreate.js
+++ src/botEvents/messageCreate.js
@@ -4,11 +4,16 @@ const { get } = require('axios');
 module.exports = async (client, message) => {
   if (message.author.bot || message.channel.type == ChannelType.DM) return;
 
+  const dbGuild = await client.db.guilds.findOne({_id: message.guild.id,});
+  const dbUser = await client.db.users.findOne({_id: message.author.id,});
+
+  if (!dbGuild) await client.db.guilds.create({ _id: message.guild.id });
+  if (!dbUser) await client.db.users.create({ _id: message.author.id });
+
   if(message.channel.id === process.env.canalId) {
     if(message.attachments.size == 0) await message.delete()
 
     const guildDb = await client.db.guilds.findOne({_id: message.guild.id });
-    const whInsta = new WebhookClient({ url: guildDb.whInsta })
 
     const rowInsta = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
@@ -33,13 +38,14 @@ module.exports = async (client, message) => {
     await message.delete()
 
     await get(guildDb.whInsta, { headers: { Authorization: 'Bot' + process.env.botToken }}).then(async (w) => {
+      const whInsta = new WebhookClient({ url: guildDb.whInsta })
+
       await whInsta.send({ username: message.author.username, avatarURL: message.author.displayAvatarURL({ dynamyc: true}),
       content: `Foto enviada por **${message.author.tag}** \`[${message.author.id}]\`.`,
       components: [rowInsta],
       files: [{attachment: message.attachments.first().url}]}).then(async (x) => { await client.db.insta.create({ _id: x.id, autorFoto: message.author.id })})
       
     }).catch(async (e) => {
-      if(e.code === 'ERR_BAD_REQUEST') {
         const whCreate = await message.channel.createWebhook({ name: client.user.username, avatar: client.user.avatarURL()})
         await client.db.guilds.findOneAndUpdate(
           { _id: message.guild.id },
@@ -51,15 +57,8 @@ module.exports = async (client, message) => {
         content: `Foto enviada por **${message.author.tag}** \`[${message.author.id}]\`.`,
         components: [rowInsta],
         files: [{attachment: message.attachments.first().url}]}).then(async (x) => { await client.db.insta.create({ _id: x.id, autorFoto: message.author.id })})
-      }
     })
   }
-  
-  const dbGuild = await client.db.guilds.findOne({_id: message.guild.id,});
-  const dbUser = await client.db.users.findOne({_id: message.author.id,});
-
-  if (!dbGuild) await client.db.guilds.create({ _id: message.guild.id });
-  if (!dbUser) await client.db.users.create({ _id: message.author.id });
 
   const botPrefixo = !dbGuild?.prefix ? process.env.botPrefix : dbGuild?.prefix;
 
