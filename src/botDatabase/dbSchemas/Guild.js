a3d09f9049455d073b517666106b2260984b2271
Linn <115900696+linntwc@users.noreply.github.com>
2022-11-12 02:14:48 -0300

    aaa

--- src/botDatabase/dbSchemas/Guild.js
+++ src/botDatabase/dbSchemas/Guild.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const guildSchema = new Schema({
   _id: { type: String },
-  prefix: { type: String, default: process.env.PREFIX },
+  prefix: { type: String, default: process.env.botPrefix },
   whInsta: { type: String }
 })
 
