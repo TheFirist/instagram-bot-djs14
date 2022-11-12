7a267f8e5c5df44b0f0e3b20f5b26addf96aa052
Linn <115900696+linntwc@users.noreply.github.com>
2022-11-12 15:57:41 -0300

    dxdd

--- src/botDatabase/dbSchemas/Guild.js
+++ src/botDatabase/dbSchemas/Guild.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const guildSchema = new Schema({
   _id: { type: String },
   prefix: { type: String, default: process.env.botPrefix },
-  whInsta: { type: String, default: 'https://discord.com/api/webhooks/1040856682777038868/blablablabla' }
+  whInsta: { type: String }
 })
 
 const Guild = model("Guild", guildSchema);
