0e79b2a7dae5a4ac16f6c3f12ce57349f8dc7936
Linn <115900696+linntwc@users.noreply.github.com>
2022-11-12 01:27:16 -0300

    ops

--- botStart.js
+++ botStart.js
@@ -1,4 +1,4 @@
 console.clear()
 require('./src/main.js');
-// require('./src/botUtils/antiCrash.js');
+require('./src/botUtils/antiCrash.js');
 console.log('🎾 | [Anti-Crash] Carregado com sucesso.')
